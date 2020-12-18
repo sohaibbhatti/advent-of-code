8bc05b8ae603ff4be9a63b311136fa3352fe934a
Sohaib Talaat Bhatti <sohaibbbhatti@gmail.com>
2020-12-18 17:59:53 +0500

    Refactor

--- day_one/expense_report.rb
+++ day_one/expense_report.rb
@@ -1,48 +1,18 @@
 MATCHING_SUM = 2020
 
-file = File.open("input.txt")
-nums = file.readlines.map(&:chomp).map(&:to_i)
-file.close
-
-nums.combination(2) do |pair|
-  first_num, second_num = pair
-  if first_num + second_num == MATCHING_SUM
-    output = first_num * second_num
-    break
-  end
-end
-
 def fetch_nums_from_input(file_name)
   file = File.open(file_name)
   content = file.readlines.map(&:chomp).map(&:to_i)
   file.close
-  return content
+  content
 end
 
-def product_of_pairs_with_matching_sum(nums)
-  output = nil
-  nums.combination(2) do |pair|
-    first_num, second_num = pair
-    if first_num + second_num == MATCHING_SUM
-      output = first_num * second_num
-      break
-    end
+def product_of_sets_with_matching_sum(nums, set_size = 2)
+  nums.combination(set_size) do |set|
+    return(set.reduce(&:*)) if set.sum == MATCHING_SUM
   end
+  nil
 end
 
-def product_of_triplets_with_matching_sum(nums)
-  output = nil
-  nums.combination(3) do |triplet|
-    first_num, second_num, third_num = triplet
-    if first_num + second_num + third_num == MATCHING_SUM
-      output = first_num * second_num * third_num
-      break
-    end
-  end
-
-  output
-end
-
-nums = fetch_nums_from_input "input.txt"
-puts product_of_triplets_with_matching_sum(nums)
-
+nums = fetch_nums_from_input 'input.txt'
+puts product_of_sets_with_matching_sum(nums, 3)
