d3095e3390f60d539c371578badfe2c5e6fa1327
Sohaib Talaat Bhatti <sohaibbbhatti@gmail.com>
2020-12-18 17:55:43 +0500

    Implement solution for part two

--- day_one/expense_report.rb
+++ day_one/expense_report.rb
@@ -1,11 +1,8 @@
-
 MATCHING_SUM = 2020
 
 file = File.open("input.txt")
 nums = file.readlines.map(&:chomp).map(&:to_i)
 file.close
-
-output = nil
 
 nums.combination(2) do |pair|
   first_num, second_num = pair
@@ -15,4 +12,37 @@ nums.combination(2) do |pair|
   end
 end
 
-puts output
+def fetch_nums_from_input(file_name)
+  file = File.open(file_name)
+  content = file.readlines.map(&:chomp).map(&:to_i)
+  file.close
+  return content
+end
+
+def product_of_pairs_with_matching_sum(nums)
+  output = nil
+  nums.combination(2) do |pair|
+    first_num, second_num = pair
+    if first_num + second_num == MATCHING_SUM
+      output = first_num * second_num
+      break
+    end
+  end
+end
+
+def product_of_triplets_with_matching_sum(nums)
+  output = nil
+  nums.combination(3) do |triplet|
+    first_num, second_num, third_num = triplet
+    if first_num + second_num + third_num == MATCHING_SUM
+      output = first_num * second_num * third_num
+      break
+    end
+  end
+
+  output
+end
+
+nums = fetch_nums_from_input "input.txt"
+puts product_of_triplets_with_matching_sum(nums)
+
